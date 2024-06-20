0ec78580543cd604eeb3a39f6952ec8fb335e600
Maksym Nahyna <maks060691@gmail.com>
2024-06-20 16:48:31 +0300

    Site name added

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
-//----------------
-import '../styles/globals.css'
-import '../fonts/stylesheet.css'
-
-
+import '../styles/globals.css';
+import '../fonts/stylesheet.css';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   const { locale } = pageProps;
+  const siteTitle = locale === 'en' ? 'Filomena' : 'Філомена';
 
   return (
-    <Layout locale={locale}>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <Layout locale={locale}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
